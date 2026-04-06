9878cea73eea0f323221499bff187694f0285592
QwiglyDee <qwiglydee@gmail.com>
2026-04-07 02:18:50 +0700

    restarted

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+> :warning: The code is work in progress
+
 # otree-advanced-apps
-Some apps for Otree with advanced features 
+
+Some apps for [oTtree](https://www.otree.org/) v6 with advanced features beyond standard capabilities
---
+++ settings.py
@@ -0,0 +1,38 @@
+from os import environ
+
+
+SESSION_CONFIGS = [
+    # dict(
+    #     name='survey',
+    #     app_sequence=['survey'],
+    #     condition="random",
+    # ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    num_demo_participants=2,
+    participation_fee=1.00,
+    real_world_currency_per_point=0.10,
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True  # makes player.payoff as points or currency
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+SECRET_KEY = '4250744705247'
