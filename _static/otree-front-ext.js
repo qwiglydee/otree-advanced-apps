a2ab65217d3d88668eb0c145a0a5f141e478cac1
QwiglyDee <qwiglydee@gmail.com>
2026-04-18 03:49:45 +0700

    upd scripts and templates

--- _static/otree-front-ext.js
+++ _static/otree-front-ext.js
@@ -4,14 +4,11 @@
  */
 
 /**
- * Wrappers for lifecycle handlers.
+ * Helpers for lifecycle handlers.
  *
  * @example
- * onLoad(startGame);
- * onSubmit(doSomething);
- *
- * function startGame() { ... }
- * function doSomething() { ... }
+ * onLoad(function startGame() { ... });
+ * onSubmit(function submitPage() { ... });
  */
 function onLoad(handler) {
     ot.onEvent("loaded", handler);
@@ -21,14 +18,11 @@ function onSubmit(handler) {
 }
 
 /**
- * Warapper for input handler.
+ * Helpers for input handler.
  *
  * @example
- * onInput('inputname', inputSomething);
- * onInputs(inputAll);
- *
- * function inputSomething(value) { ... }
- * function inputAll(name, value) { ... }
+ * onInput('inputname', function inputSomething(value) { ... });
+ * onInputs(function inputAll(name, value) { ... });
  */
 function onInputs(handler) {
     ot.onEvent("input", (e) => handler(e.detail.name, e.detail.value));
@@ -38,14 +32,11 @@ function onInput(name, handler) {
 }
 
 /**
- * Warapper for timer handlers.
+ * Helpers for timer handlers.
  *
  * @example
- * onTimer(name, handleTimer);
- * onTimers(handleTimers));
- *
- :* function handleTimer(elapsed, counter) { ... }
- * function handleTimers(name, elapsed, counter) { ... }
+ * onTimer('timername', function handleTimer(elapsed, counter) { ... });
+ * onTimers(function handleTimers(name, elapsed, counter) { ... });
  */
 function onTimers(handler) {
     ot.onEvent("timer", (e) => handler(e.detail.name, e.detail.elapsed, e.detail.count));
@@ -55,12 +46,10 @@ function onTimer(name, handler) {
 }
 
 /**
- * Wrapper for built-in oTree page timer.
+ * Helper for built-in oTree page timer.
  *
  * @example
- * onCountdown(handleCountdown);
- *
- * function handleCountdown(remaining_seconds) { ... }
+ * onCountdown(function handleCountdown(remaining_seconds) { ... });
  */
 function onCountdown(handler) {
     ot.onEvent("countdown", (e) => handler(e.detail.remaining));
@@ -71,11 +60,19 @@ if (document.querySelector(".otree-timer")) {
         ot.triggerEvent("countdown", { remaining: e.offset.totalSeconds });
     });
 }
+/**
+ * Helper for built-in oTree update event.
+ *
+ * @example
+ * onUpdate('varname', function updateVar(var_value) { ... })
+ * onUpdate('objname.fieldname', function updateField(field_value) { ... })
+ * onUpdate('objname.*', function updateObj(obj_value) { ... })
+ */
 
 function onUpdate(varname, handler) {
     ot.onEvent(
         "update",
         (e) => e.detail.changes.affect(varname),
-        (e) => handler(e.detail.changes.extract(varname))
+        (e) => handler(e.detail.changes.extract(varname)),
     );
 }
