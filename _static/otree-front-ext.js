aa1dee3f167fb351aed11bb61bb6c1a8b9b9443f
QwiglyDee <qwiglydee@gmail.com>
2026-04-30 23:13:02 +0700

    refact scripts namespace

--- _static/otree-front-ext.js
+++ _static/otree-front-ext.js
@@ -3,153 +3,162 @@
  * by qwiglydee@gmail.com
  */
 
-/**
- * Helpers for lifecycle handlers.
- *
- * @example
- * onLoad(function startGame() { ... });
- * onSubmit(function submitPage() { ... });
- */
-function onLoad(handler) {
-    ot.onEvent("loaded", handler);
-}
-function onSubmit(handler) {
-    ot.onEvent("submitted", handler);
-}
+(function (ot) {
+    if (ot === undefined) throw Error("otree-front-ext requires otree-front");
 
-/**
- * Helpers for input handler.
- *
- * @example
- * onInput('inputname', function inputSomething(value) { ... });
- * onInputs(function inputAll(name, value) { ... });
- */
-function onInputs(handler) {
-    ot.onEvent("input", (e) => handler(e.detail.name, e.detail.value));
-}
-function onInput(name, handler) {
-    ot.onEvent("input", name, (e) => handler(e.detail.value));
-}
+    /**
+     * Helpers for lifecycle handlers.
+     *
+     * @example
+     * onLoad(function startGame() { ... });
+     * onSubmit(function submitPage() { ... });
+     */
+    function onLoad(handler) {
+        ot.onEvent("loaded", handler);
+    }
+    function onSubmit(handler) {
+        ot.onEvent("submitted", handler);
+    }
 
-/**
- * Helpers for timer handlers.
- *
- * @example
- * onTimer('timername', function handleTimer(elapsed, counter) { ... });
- * onTimers(function handleTimers(name, elapsed, counter) { ... });
- */
-function onTimers(handler) {
-    ot.onEvent("timer", (e) => handler(e.detail.name, e.detail.elapsed, e.detail.count));
-}
-function onTimer(name, handler) {
-    ot.onEvent("timer", name, (e) => handler(e.detail.elapsed, e.detail.count));
-}
+    /**
+     * Helpers for input handler.
+     *
+     * @example
+     * onInput('inputname', function inputSomething(value) { ... });
+     * onInputs(function inputAll(name, value) { ... });
+     */
+    function onInputs(handler) {
+        ot.onEvent("input", (e) => handler(e.detail.name, e.detail.value));
+    }
+    function onInput(name, handler) {
+        ot.onEvent("input", name, (e) => handler(e.detail.value));
+    }
 
-/**
- * Helper for built-in oTree page timer.
- *
- * @example
- * onCountdown(function handleCountdown(remaining_seconds) { ... });
- */
-function onCountdown(handler) {
-    ot.onEvent("countdown", (e) => handler(e.detail.remaining));
-}
+    /**
+     * Helpers for timer handlers.
+     *
+     * @example
+     * onTimer('timername', function handleTimer(elapsed, counter) { ... });
+     * onTimers(function handleTimers(name, elapsed, counter) { ... });
+     */
+    function onTimers(handler) {
+        ot.onEvent("timer", (e) => handler(e.detail.name, e.detail.elapsed, e.detail.count));
+    }
+    function onTimer(name, handler) {
+        ot.onEvent("timer", name, (e) => handler(e.detail.elapsed, e.detail.count));
+    }
 
-if (document.querySelector(".otree-timer")) {
-    $(".otree-timer__time-left").on("update.countdown", function (e) {
-        ot.triggerEvent("countdown", { remaining: e.offset.totalSeconds });
+    /**
+     * Helper for built-in oTree page timer.
+     *
+     * @example
+     * onCountdown(function handleCountdown(remaining_seconds) { ... });
+     */
+    function onCountdown(handler) {
+        ot.onEvent("countdown", (e) => handler(e.detail.remaining));
+    }
+
+    if (document.querySelector(".otree-timer")) {
+        $(".otree-timer__time-left").on("update.countdown", function (e) {
+            ot.triggerEvent("countdown", { remaining: e.offset.totalSeconds });
+        });
+    }
+    /**
+     * Helper for built-in oTree update event.
+     *
+     * @example
+     * onUpdate('vars.name', function updateVar(var_value) { ... })
+     * onUpdate('vars.objname.fieldname', function updateField(field_value) { ... })
+     * onUpdate('vars.objname.*', function updateObj(obj_value) { ... })
+     */
+
+    function onUpdate(varname, handler) {
+        if (!varname.startsWith("vars.")) throw Error("onUpdate: invalid var name, expecting `vars.` ");
+        let varref = varname.slice(5);
+        ot.onEvent(
+            "update",
+            (e) => e.detail.changes.affect(varref),
+            (e) => handler(e.detail.changes.extract(varref)),
+        );
+    }
+
+    /** switching directives */
+
+    class otDisplay extends ot.ContentDirective {
+        params = { val: { attr: "ot-display", default: false } };
+
+        render() {
+            let toggle = ot.isVoid(this.val) || this.val == false;
+            this.elem.toggleAttribute("hidden", toggle);
+            autofocus(this.elem);
+        }
+    }
+    ot.attachDirective(otDisplay, "[ot-display]");
+
+    class otVisible extends ot.ContentDirective {
+        params = { val: { attr: "ot-visible", default: false } };
+
+        render() {
+            let toggle = ot.isVoid(this.val) || this.val == false;
+            this.elem.toggleAttribute("invisible", toggle);
+            this.elem.style.visibility = toggle ? "hidden" : null;
+            autofocus(this.elem);
+        }
+    }
+    ot.attachDirective(otVisible, "[ot-visible]");
+
+    class otEnable extends ot.ContentDirective {
+        params = { val: { attr: "ot-enable", default: false } };
+
+        render() {
+            let toggle = ot.isVoid(this.val) || this.val == false;
+            this.elem.toggleAttribute("disabled", toggle);
+            autofocus(this.elem);
+        }
+    }
+    ot.attachDirective(otEnable, "[ot-enable]");
+
+    function autofocus(elem) {
+        if (elem.hasAttribute("hidden") || elem.hasAttribute("invisible") || elem.hasAttribute("disabled")) return;
+
+        if (elem.hasAttribute("autofocus")) {
+            // refocus when re-enabled
+            elem.focus();
+        } else {
+            // nested inputs of re-displayed block
+            let nested = elem.querySelector("[autofocus]:not([disabled]):not([hidden]):not([invisible])");
+            if (nested) nested.focus();
+        }
+    }
+
+    class otRequired extends ot.ContentDirective {
+        params = { val: { attr: "ot-required", default: false } };
+
+        render() {
+            let toggle = !ot.isVoid(this.val) && this.val == true;
+            this.elem.toggleAttribute("required", toggle);
+        }
+    }
+    ot.attachDirective(otRequired, "[ot-required]");
+
+    class otSelected extends ot.ContentDirective {
+        params = { val: { attr: "ot-selected", default: false } };
+
+        render() {
+            let toggle = !ot.isVoid(this.val) && this.val == true;
+            this.elem.toggleAttribute("selected", toggle);
+        }
+    }
+    ot.attachDirective(otSelected, "[ot-selected]");
+
+    window.ot = Object.assign({}, window.ot, {
+        onLoad,
+        onSubmit,
+        onInput,
+        onInputs,
+        onTimer,
+        onTimers,
+        onCountdown,
+        onUpdate,
     });
-}
-/**
- * Helper for built-in oTree update event.
- *
- * @example
- * onUpdate('vars.name', function updateVar(var_value) { ... })
- * onUpdate('vars.objname.fieldname', function updateField(field_value) { ... })
- * onUpdate('vars.objname.*', function updateObj(obj_value) { ... })
- */
-
-function onUpdate(varname, handler) {
-    if (!varname.startsWith("vars.")) throw Error("onUpdate: invalid var name, expecting `vars.` ");
-    let varref = varname.slice(5);
-    ot.onEvent(
-        "update",
-        (e) => e.detail.changes.affect(varref),
-        (e) => handler(e.detail.changes.extract(varref)),
-    );
-}
-
-/** switching directives */
-
-class otDisplay extends ot.ContentDirective {
-    params = { val: { attr: "ot-display", default: false } };
-
-    render() {
-        let toggle = ot.isVoid(this.val) || this.val == false;
-        this.elem.toggleAttribute("hidden", toggle);
-        autofocus(this.elem);
-    }
-}
-ot.attachDirective(otDisplay, "[ot-display]");
-
-class otVisible extends ot.ContentDirective {
-    params = { val: { attr: "ot-visible", default: false } };
-
-    render() {
-        let toggle = ot.isVoid(this.val) || this.val == false;
-        this.elem.toggleAttribute("invisible", toggle);
-        this.elem.style.visibility = toggle ? "hidden" : null;
-        autofocus(this.elem);
-    }
-}
-ot.attachDirective(otVisible, "[ot-visible]");
-
-class otEnable extends ot.ContentDirective {
-    params = { val: { attr: "ot-enable", default: false } };
-
-    render() {
-        let toggle = ot.isVoid(this.val) || this.val == false;
-        this.elem.toggleAttribute("disabled", toggle);
-        autofocus(this.elem);
-    }
-}
-ot.attachDirective(otEnable, "[ot-enable]");
-
-function autofocus(elem) {
-    if (elem.hasAttribute("hidden") || elem.hasAttribute("invisible") || elem.hasAttribute("disabled")) return;
-
-    if (elem.hasAttribute("autofocus")) {
-        // refocus when re-enabled
-        elem.focus();
-    } else {
-        // nested inputs of re-displayed block
-        let nested = elem.querySelector("[autofocus]:not([disabled]):not([hidden]):not([invisible])");
-        if (nested) nested.focus();
-    }
-}
-
-class otRequired extends ot.ContentDirective {
-    params = { val: { attr: "ot-required", default: false } };
-
-    render() {
-        let toggle = !ot.isVoid(this.val) && this.val == true;
-        this.elem.toggleAttribute("required", toggle);
-    }
-}
-ot.attachDirective(otRequired, "[ot-required]");
-
-class otSelected extends ot.ContentDirective {
-    params = { val: { attr: "ot-selected", default: false } };
-
-    render() {
-        let toggle = !ot.isVoid(this.val) && this.val == true;
-        this.elem.toggleAttribute("selected", toggle);
-    }
-}
-ot.attachDirective(otSelected, "[ot-selected]");
-
-function classMap(stylemap) {
-    return Object.entries(stylemap)
-        .filter(([k, v]) => v == true)
-        .map(([k, v]) => k);
-}
+})(window.ot);
--- _static/otree-front-live.js
+++ _static/otree-front-live.js
@@ -2,47 +2,56 @@
  * Utils for type-based live message
  * by qwiglydee@gmail.com
  */
+(function (ot) {
+    if (window.ot === undefined) throw Error("otree-front-live requires otree-front");
+    if (window.liveSocket === undefined) throw Error("otree-front-live requires live page");
 
-/**
- * Send a live message with a specified type and possible payload.
- *
- * @example
- *   sendLive("foo");
- *   sendLive("bar", { ... } );
- *
- * @param {string} type type of the message
- * @param {object} [data] message payload
- */
-function sendLive(type, data) {
-    if (data) liveSend({ type, ...data });
-    else liveSend({ type });
-}
-
-/**
- * Install handler for a live message .
- *
- * @example
- *  onLive('some_type', liveSomething);
- *
- *  function liveSomething(data) {
- *    // handle data
- *  }
- */
-function onLive(name, handler) {
-    ot.onEvent("live", name, (e) => handler(e.detail.data));
-}
-
-window.liveSocket.onmessage = function (e) {
-    console.debug("live:", e.data);
-    try {
-        let data = JSON.parse(e.data);
-        if (data.otree_success === false) throw Error("Server error");
-        data = data.live_method_payload;
-        if (!ot.isObject(data) || !data.type) throw Error("Bogus data received");
-        ot.emitEvent("live", { name: data.type, data });
-    } catch (e) {
-        console.error(e);
-        window.alert("Application error occured. The page terminates.");
-        ot.submitPage();
+    /**
+     * Send a live message with a specified type and possible payload.
+     *
+     * @example
+     *   sendLive("foo");
+     *   sendLive("bar", { ... } );
+     *
+     * @param {string} type type of the message
+     * @param {object} [data] message payload
+     */
+    function sendLive(type, data) {
+        if (data) liveSend({ type, ...data });
+        else liveSend({ type });
     }
-};
+
+    /**
+     * Install handler for a live message .
+     *
+     * @example
+     *  onLive('some_type', liveSomething);
+     *
+     *  function liveSomething(data) {
+     *    // handle data
+     *  }
+     */
+    function onLive(name, handler) {
+        ot.onEvent("live", name, (e) => handler(e.detail.data));
+    }
+
+    window.liveSocket.onmessage = function (e) {
+        // console.debug("live:", e.data);
+        try {
+            let data = JSON.parse(e.data);
+            if (data.otree_success === false) throw Error("Server error");
+            data = data.live_method_payload;
+            if (!ot.isObject(data) || !data.type) throw Error("Bogus data received");
+            ot.emitEvent("live", { name: data.type, data });
+        } catch (e) {
+            console.error(e);
+            window.alert("Application error occured. The page terminates.");
+            ot.submitPage();
+        }
+    };
+
+    window.ot = Object.assign({}, window.ot, {
+        onLive,
+        sendLive,
+    });
+})(window.ot);
